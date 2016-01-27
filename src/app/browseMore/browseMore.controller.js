66c6420779297f5f50f50031559404343ce9109a
Ashish Yadav <ashish.yadav@magicsw.com>
2016-01-27 10:42:15 +0530

    Source Code upload | new browseMore add ons

--- src/app/browseMore/browseMore.controller.js
+++ src/app/browseMore/browseMore.controller.js
@@ -12,10 +12,30 @@ export class BrowseController {
       data:{'searchobject':{}}
     }).then(function successCallback(response) {
       $scope.productDetails = response.data.productdetail;
-      $log.debug(1,$scope.productDetails)
+      $log.debug(1,response.data)
+
     }, function errorCallback(err) {
-     $log.debug(err)
+     $log.debug(err);
+      alert("sorry service unavailable");
     });
+
+
+   /* var contentSelfIcons = {
+      video : "../assets/images/video_icon.svg",
+      audio : "../assets/images/audio_icon.svg",
+      epub  : "../assets/images/epub_icon.svg",
+      ebook : "../assets/images/ebook_icon.svg",
+      simulation :"../assets/images/simulator_icon.svg",
+      game : "../assets/images/game_icon.svg",
+      assessment:"../assets/images/exam_icon.svg",
+      pdf:"../assets/images/pdf_icon.svg",
+      EXT_RESOURCE:"../assets/images/ext_resource-icon.svg"
+    }
+    */
+
+
+
+
 
   }
 }
