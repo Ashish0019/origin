3e229d5681079e2a2098c2f2965cf2599e62fd32
Ashish Yadav <ashish.yadav@magicsw.com>
2016-02-01 19:42:51 +0530

    Source Code upload | library and product detail page fixes

--- src/app/product/product.controller.js
+++ src/app/product/product.controller.js
@@ -11,17 +11,27 @@ export class ProductController {
     if (!_.isEmpty(detail)) {
       this.author = detail.author;
       this.showDetails = true;
-      this.info.title = detail.title;
-      this.info.Content = "in" +" "  + "<b><u>" + detail.subject + "</u></b>"  +" "+"by" + " "
-        +"<b><u>" + detail.author + "</u></b>";
-      this.info.Grades = " : " + detail.meta.gradeFrom +" - " + detail.meta.gradeTo;
-      this.info.Publisher = " : " + "Magic publisher";
-      this.info.Type = " : " + detail.productType || " : Youtube Video" ;
-      this.description = detail.meta.description;
-      this.info.Image = detail.coverImage;
-      this.videoPath="http://www.youtube.com/embed/"+ detail.id;
-      this.yVideo = $sce.trustAsResourceUrl(this.videoPath);
-      $log.debug(this.yvideo)
+
+      if(detail.author== "Youtube"){
+        this.info.title = detail.title;
+        this.info.Content = "in" +" "  + "<b><u>" + detail.subject + "</u></b>"  +" "+"by" + " "
+          +"<b><u>" + detail.author + "</u></b>";
+        this.description = detail.meta.description;
+        this.info.Type = " : Youtube Video";
+        this.videoPath="http://www.youtube.com/embed/"+ detail.id;
+        this.yVideo = $sce.trustAsResourceUrl(this.videoPath);
+        $log.debug(this.yvideo)
+      }
+      else {
+        this.info.title = detail.title;
+        this.info.Content = "in" + " " + "<b><u>" + detail.subject + "</u></b>" + " " + "by" + " "
+          + "<b><u>" + detail.author + "</u></b>";
+        this.info.Grades = " : " + detail.meta.gradeFrom + " - " + detail.meta.gradeTo;
+        this.info.Publisher = " : " + "Magic publisher";
+        this.info.Type = " : " + detail.productType;
+        this.description = detail.meta.description;
+        this.info.Image = detail.coverImage;
+      }
     }
   }
 }
