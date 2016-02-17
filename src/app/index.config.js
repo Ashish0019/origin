f743d5bdd5fc628f2342066045b609b7be7b50e6
ashish0019 <ashishyadav13@gmail.com>
2016-02-17 16:16:52 +0530

    Origin | issues

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -4,6 +4,6 @@ export function config ($logProvider, reCAPTCHAProvider, $serviceProvider) {
   reCAPTCHAProvider.setPublicKey('6LdUu_cSAAAAAJT-SnxZm_EL_NwazPuCwgfb70Wo');
   reCAPTCHAProvider.setOptions({theme: 'blackglass'});
 
-  $serviceProvider.setEnv('set', 'staging');
+  $serviceProvider.setEnv('set', 'production');
   $serviceProvider.setToken('ORIGIN_MAGICBOX_TOKEN'/* || 'MAGICBOX_DEMO_TOKEN'*/);
 }
